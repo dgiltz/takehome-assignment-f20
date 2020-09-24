a78ca725782cc1742063622cc9aff11c1c4baef1
dgiltz <70062287+dgiltz@users.noreply.github.com>
2020-09-24 18:33:21 -0400

    Update Counter.vue

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -2,8 +2,7 @@
   <div>
     <p>I'm a Counter!</p>
     <Counter v-bind:num=0>
-      <p>The count is {{count}}
-      </p>
+      The count is {{count}}
     </Counter>
     <div>
       <button v-on:click="count++">Increase</button>

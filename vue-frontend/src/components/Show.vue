e67e5e4b9ffa326cb90f016f1e06825c43041fa1
dgiltz <70062287+dgiltz@users.noreply.github.com>
2020-09-24 16:03:17 -0400

    Getting the Counter to Appear

--- vue-frontend/src/components/Show.vue
+++ vue-frontend/src/components/Show.vue
@@ -2,7 +2,7 @@
   <!-- PART 4: Add your code here -->
   <div>
     <p> Name: {{name}} </p>
-    <Counter :num=this.episodes_seen></Counter>
+    <p> <Counter :num=this.episodes_seen></Counter> </p>
   </div>
 </template>
 

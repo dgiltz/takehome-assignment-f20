3da088b96c0ffd45b80622c61fc6dfe72e5a17c8
dgiltz <70062287+dgiltz@users.noreply.github.com>
2020-09-24 12:43:17 -0400

    Update Counter.vue

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -1,7 +1,11 @@
 <template>
-  <div id='Counter'>
+  <div>
     <p>I'm a Counter!</p>
     <p>The count is {{count}}</p>
+    <div>
+      <button v-on:click="count++">Increase</button>
+      <button v-on:click="count--">Decrease</button>
+    </div>
     <Counter></Counter>
   </div>
 </template>
@@ -13,8 +17,8 @@ export default {
       count: 0
     };
   },
-  template: '<button v-on:click="count++">Increase</button>' 
-  // <button v-on:click="count--">Decrease</button>'
+  template: '<button v-on:click="count++">Increase</button>', 
+  template: '<button v-on:click="count--">Decrease</button>'
 };
 </script>
 

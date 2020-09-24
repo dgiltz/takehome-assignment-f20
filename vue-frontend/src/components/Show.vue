3c1140eecae1b5b7986856e87a819fb153007e63
dgiltz <70062287+dgiltz@users.noreply.github.com>
2020-09-24 15:56:04 -0400

    Fix Errors

--- vue-frontend/src/components/Show.vue
+++ vue-frontend/src/components/Show.vue
@@ -2,7 +2,7 @@
   <!-- PART 4: Add your code here -->
   <div>
     <p> Name: {{name}} </p>
-    <Counter num={episodes_seen}></Counter>
+    <Counter num={{episodes_seen}}></Counter>
   </div>
 </template>
 

d76282e03440caa9c3e01f667c0dcf1b867c16b2
dgiltz <70062287+dgiltz@users.noreply.github.com>
2020-09-24 18:43:19 -0400

    Update Counter.vue

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -16,7 +16,7 @@ export default {
   props: {'num': Number},
   data() {
     return {
-      count: this.props.num
+      count: {{num}}
     };
   },
   template: '<p> The count is {{count}} </p>',

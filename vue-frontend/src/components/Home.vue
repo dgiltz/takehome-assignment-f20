d7e91b8d901a4018a382c8b7fe03e998ee4d156d
dgiltz <70062287+dgiltz@users.noreply.github.com>
2020-09-24 10:42:48 -0400

    Part One (Just Kidding) Second Try

--- vue-frontend/src/components/Home.vue
+++ vue-frontend/src/components/Home.vue
@@ -2,7 +2,11 @@
   <div>
     <!-- PART 1: Pass in a "complete" prop here --> 
     <Instructions />
-    props = ['complete']
+    Vue.component('part one', {
+      props: {
+        'complete': Boolean
+      }
+    })
     <!-- PART 4: Modify the Show component to accept all of these props -->
     <Show
       v-for="show in shows"
